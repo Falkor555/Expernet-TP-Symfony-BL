1e972178dce0c84bf162770d1ca19d045c47412e
Falkor555 <loic.botsy@hotmail.com>
2026-01-22 16:14:13 +0400

    Ajout de la validation du panier vide avant création de commande et mise à jour de l'interface en français
    - Vérification que le panier n'est pas vide avant d'accéder au formulaire de commande
    - Double validation lors de la soumission du formulaire pour éviter les commandes vides
    - Création automatique des lignes de commande à partir du contenu du panier
    - Vidage du panier après validation réussie de la commande
    - Traduction des messages d'interface utilisateur en français
    - Ajout de messages flash informatifs pour l'utilisateur

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Commande;
+use App\Entity\LigneCommande;
 use App\Form\CommandeType;
 use App\Repository\CommandeRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -26,25 +27,58 @@ final class CommandeController extends AbstractController
     #[Route('/new', name: 'app_commande_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager, PanierService $panierService): Response
     {
+        // Vérifier que le panier n'est pas vide
+        $panierItems = $panierService->getFullPanier();
+        if (empty($panierItems)) {
+            $this->addFlash('warning', 'Votre panier est vide ! Ajoutez des produits avant de créer une commande.');
+            return $this->redirectToRoute('app_catalogue');
+        }
+
         $commande = new Commande();
         $form = $this->createForm(CommandeType::class, $commande);
         $form->handleRequest($request);
     
         if ($form->isSubmitted() && $form->isValid()) {
+            // Revérifier que le panier n'est pas vide avant de valider
+            $panierItems = $panierService->getFullPanier();
+            if (empty($panierItems)) {
+                $this->addFlash('error', 'Votre panier est vide ! Impossible de créer une commande.');
+                return $this->redirectToRoute('app_catalogue');
+            }
+
             $commandeForm = $form->getData();
             $commandeForm->setDateCommande(new \DateTime());
             $commandeForm->setTotal((string)$panierService->getTotal());
             $commandeForm->setStatut('En attente de validation');
             $commandeForm->setUtilisateur($this->getUser());
+
+            // Créer les lignes de commande à partir du panier
+            foreach ($panierItems as $item) {
+                $ligneCommande = new LigneCommande();
+                $ligneCommande->setProduit($item['produit']);
+                $ligneCommande->setQuantite($item['quantite']);
+                $ligneCommande->setPrixUnitaire($item['produit']->getPrix());
+                $ligneCommande->setCommande($commandeForm);
+                
+                $commandeForm->addLignesCommande($ligneCommande);
+                $entityManager->persist($ligneCommande);
+            }
+
             $entityManager->persist($commandeForm);
             $entityManager->flush();
     
-            return $this->redirectToRoute('app_commande_index', [], Response::HTTP_SEE_OTHER);
+            // Vider le panier après validation
+            $panierService->clear();
+    
+            $this->addFlash('success', 'Votre commande a été créée avec succès !');
+            return $this->redirectToRoute('app_commande_show', ['id' => $commandeForm->getId()], Response::HTTP_SEE_OTHER);
         }
     
         return $this->render('commande/new.html.twig', [
             'commande' => $commande,
             'form' => $form,
+            'panier' => $panierItems,
+            'total' => $panierService->getTotal(),
         ]);
     }
 
